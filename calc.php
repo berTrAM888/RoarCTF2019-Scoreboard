d2f7b686faf665bd6a39df81a05011351b975e8c
bertram888 <928921254@qq.com>
2019-10-14 16:37:38 +0800

    ADD Source Data & Final Data

---
+++ calc.php
@@ -0,0 +1,14 @@
+<?php
+
+function calculate($solvedCount, $baseScore = 1000, $minimumScore = 10){
+        if ($solvedCount === 1){
+            return $baseScore;
+        }
+        $score = ($baseScore / 1000) * (-28730 * pow($solvedCount, 0.007236) + 29840);
+        if ($score <= $minimumScore){
+            return $minimumScore;
+        }
+        return round($score, 0);
+}
+
+echo calculate((int)$argv[1]);
